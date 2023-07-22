fd13e2be1452a108316a761bffc486988f293855
tensa2205 <borlandi_diego@hotmail.com>
2023-07-22 14:53:12 -0300

    Mejorada la query para obtener retiros en fecha

--- src/main/java/com/devbanksu/dev/movimientos/MovimientoRepository.java
+++ src/main/java/com/devbanksu/dev/movimientos/MovimientoRepository.java
@@ -8,8 +8,7 @@ import java.util.Date;
 
 public interface MovimientoRepository extends JpaRepository<Movimiento, Long> {
 
-    //TODO Revisar si puedo devolver directamente un booleano
-    @Query("SELECT SUM(m.valor) FROM Movimiento m WHERE m.cuenta.id = :idCuenta AND m.tipo = 'RETIRO' AND m.fecha = :fecha")
+    @Query("SELECT IFNULL(SUM(m.valor), 0) FROM Movimiento m WHERE m.cuenta.id = :idCuenta AND m.tipo = com.devbanksu.dev.movimientos.TipoMovimiento.RETIRO AND m.fecha = :fecha")
     public BigDecimal obtenerRetirosEnFecha(Long idCuenta, Date fecha);
 
 }

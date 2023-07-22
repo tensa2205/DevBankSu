45349010d9112eb61f6d26e4c26ef6251f698d19
tensa2205 <borlandi_diego@hotmail.com>
2023-07-22 14:14:17 -0300

    Query en repository para chequear limite diario

--- src/main/java/com/devbanksu/dev/movimientos/MovimientoRepository.java
+++ src/main/java/com/devbanksu/dev/movimientos/MovimientoRepository.java
@@ -1,6 +1,15 @@
 package com.devbanksu.dev.movimientos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.math.BigDecimal;
+import java.util.Date;
 
 public interface MovimientoRepository extends JpaRepository<Movimiento, Long> {
+
+    //TODO Revisar si puedo devolver directamente un booleano
+    @Query("SELECT SUM(m.valor) FROM Movimiento m WHERE m.cuenta.id = :idCuenta AND m.tipo = 'RETIRO' AND m.fecha = :fecha")
+    public BigDecimal obtenerRetirosEnFecha(Long idCuenta, Date fecha);
+
 }

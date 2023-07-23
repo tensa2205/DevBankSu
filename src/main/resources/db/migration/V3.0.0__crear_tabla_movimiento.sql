40b77142ca3237008eefc3a774d3d4da9f94a177
tensa2205 <borlandi_diego@hotmail.com>
2023-07-23 17:43:12 -0300

    Agregado non null en .sql

--- src/main/resources/db/migration/V3.0.0__crear_tabla_movimiento.sql
+++ src/main/resources/db/migration/V3.0.0__crear_tabla_movimiento.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS movimiento (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     cuenta_id BIGINT NOT NULL,
-    fecha DATE,
-    tipo VARCHAR(255),
-    valor DECIMAL(19, 2),
-    saldo_disponible DECIMAL(19, 2),
+    fecha DATE NOT NULL,
+    tipo VARCHAR(255) NOT NULL,
+    valor DECIMAL(19, 2) NOT NULL,
+    saldo_disponible DECIMAL(19, 2) NOT NULL,
     FOREIGN KEY (cuenta_id) REFERENCES cuenta(id)
 );

c2157a4f96389be0daddd4dd4fe9041a186bfe1a
tensa2205 <borlandi_diego@hotmail.com>
2023-07-23 00:49:38 -0300

    Fix comparación de fecha en repository de movimiento

--- src/main/java/com/devbanksu/dev/movimientos/MovimientoRepository.java
+++ src/main/java/com/devbanksu/dev/movimientos/MovimientoRepository.java
@@ -8,7 +8,7 @@ import java.util.Date;
 
 public interface MovimientoRepository extends JpaRepository<Movimiento, Long> {
 
-    @Query("SELECT IFNULL(SUM(m.valor), 0) FROM Movimiento m WHERE m.cuenta.id = :idCuenta AND m.tipo = com.devbanksu.dev.movimientos.TipoMovimiento.RETIRO AND m.fecha = :fecha")
+    @Query("SELECT IFNULL(SUM(m.valor), 0) FROM Movimiento m WHERE m.cuenta.id = :idCuenta AND m.tipo = com.devbanksu.dev.movimientos.TipoMovimiento.RETIRO AND CAST(m.fecha AS date) = CAST(:fecha AS date)")
     public BigDecimal obtenerRetirosEnFecha(Long idCuenta, Date fecha);
 
 }

4df499e0ea7903e1cb3dbf62bf5184e7bb66a8b5
tensa2205 <borlandi_diego@hotmail.com>
2023-07-23 17:58:46 -0300

    Agregado advice para Constraint exception

--- src/main/java/com/devbanksu/dev/advice/ManejadorGlobalDeException.java
+++ src/main/java/com/devbanksu/dev/advice/ManejadorGlobalDeException.java
@@ -3,11 +3,15 @@ package com.devbanksu.dev.advice;
 import com.devbanksu.dev.exceptions.EntidadNoEncontradaException;
 import com.devbanksu.dev.exceptions.LimiteDiarioException;
 import com.devbanksu.dev.exceptions.SaldoNoDisponibleException;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestControllerAdvice
 public class ManejadorGlobalDeException {
@@ -34,5 +38,16 @@ public class ManejadorGlobalDeException {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     String manejarIllegalArgument(IllegalArgumentException ex) {
         return ex.getMessage();
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    List<String> manejarConstraint(ConstraintViolationException ex) {
+        List<String> errores = new ArrayList<>();
+
+        for (ConstraintViolation<?> violation : ex.getConstraintViolations()) {
+            errores.add(violation.getMessage());
+        }
+        return errores;
     }
 }
